d23f9821154b1b1c9077f629361d0efe53830f0f
mohsin-riad <morteen1997@gmail.com>
2020-09-18 14:24:33 +0600

    add assets, include, css, fonts, js, dashboard.php

---
+++ include/link.php
@@ -0,0 +1,15 @@
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.css" rel="stylesheet">
+<link href="css/elegant-icons-style.css" rel="stylesheet" />
+<link href="css/font-awesome.min.css" rel="stylesheet" />
+<link href="assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+<link href="assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+<link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+<link href="css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+<link rel="stylesheet" href="css/fullcalendar.css">
+<link href="css/widgets.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/style-responsive.css" rel="stylesheet" />
+<link href="css/xcharts.min.css" rel=" stylesheet">
+<link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
