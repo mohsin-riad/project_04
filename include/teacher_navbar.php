efb8b3b00573554190a4abab54d69cc0bba6c18a
mohsin-riad <morteen1997@gmail.com>
2020-09-26 22:13:08 +0600

    add teacher_navbar.php, teacher folder modification

---
+++ include/teacher_navbar.php
@@ -0,0 +1,29 @@
+<header class="header dark-bg">
+    <div class="toggle-nav">
+        <div class="icon-reorder tooltips" data-original-title="Toggle Navigation" data-placement="bottom"><i class="icon_menu"></i></div>
+    </div>
+    <a href="dashboard.php" class="logo">Teacher<span class="lite">Panel</span></a>
+    <div class="nav search-row" id="top_menu">
+        <ul class="nav top-menu">
+        </ul>
+    </div>
+    <div class="top-nav notification-row">
+        <ul class="nav pull-right top-menu">
+            <li class="dropdown">
+                <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                    <span class="profile-ava">
+                        <img alt="" src="img/avatar1_small.jpg">
+                    </span>
+                    <span class="username">User: Teacher</span>
+                    <b class="caret"></b>
+                </a>
+                <ul class="dropdown-menu extended logout">
+                    <div class="log-arrow-up"></div>
+                    <li>
+                        <a href="../logout.php"><i class="icon_key_alt"></i> Log Out</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</header>
--- teacher/dashboard.php
+++ teacher/dashboard.php
@@ -21,7 +21,7 @@
   </head>
   <body>
     <section id="container" class="">
-      <?php include '../include/navbar.php' ?>
+      <?php include '../include/teacher_navbar.php' ?>
       <?php include '../include/teacher_sidebar.php' ?>
       <section id="main-content">
         <section class="wrapper">
@@ -34,63 +34,9 @@
               </ol>
             </div>
           </div>
-            <div class="col-md-6 portlets">
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  <div class="pull-left">Quick Post</div>
-                  <div class="clearfix"></div>
-                </div>
-                <div class="panel-body">
-                  <div class="padd">
-                    <div class="form quick-post">
-                      <form class="form-horizontal">
-                        <div class="form-group">
-                          <label class="control-label col-lg-2" for="title">Title</label>
-                          <div class="col-lg-10">
-                            <input type="text" class="form-control" id="title">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-lg-2" for="content">Content</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" id="title">
-                          </div>
-                        </div>
-                        <div class="form-group">
-                          <label class="control-label col-lg-2">Category</label>
-                          <div class="col-lg-10">
-                            <select class="form-control">
-                              <option value="">- Choose Cateogry -</option>
-                              <option value="1">General</option>
-                              <option value="2">News</option>
-                              <option value="3">Media</option>
-                              <option value="4">Funny</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div class="form-group">
-                          <label class="control-label col-lg-2" for="tags">Tags</label>
-                          <div class="col-lg-10">
-                            <input type="text" class="form-control" id="tags">
-                          </div>
-                        </div>
-                        <div class="form-group">
-                          <div class="col-lg-offset-2 col-lg-9">
-                            <button type="submit" class="btn btn-primary">Publish</button>
-                            <button type="submit" class="btn btn-danger">Save Draft</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
-                          </div>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </section>
       </section>
     </section>
     <?php include '../include/script.php' ?>
-    </body>
-  </html>
+  </body>
+</html>
