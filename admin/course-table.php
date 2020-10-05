707489f529e58f7c0a2843218489db239836aa92
mohsin-riad <morteen1997@gmail.com>
2020-10-05 12:40:17 +0600

    mod: admin/course-table.php

--- admin/course-table.php
+++ admin/course-table.php
@@ -36,57 +36,57 @@
               </ol>
             </div>
           </div>
-            <div class='row'>
-                <table class="table table-dark table-striped">
-                    <thead>
-                        <th>Name</th>
-                        <th>Code</th>
-                        <th>Type</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                        <?php 
-                            $qry="SELECT * FROM `courses`";
-                            $r=mysqli_query($conn,$qry);
-                            while($row=mysqli_fetch_array($r)){ ?>
-                                <tr>
-                                    <td> <?php echo $row['name']?> </td>
-                                    <td> <?php echo $row['code']?> </td>
-                                    <td> <?php echo $row['type']?> </td>
-                                    <td>
-                                        <a class="btn btn-primary" href="edit-courses.php?id=<?php echo $row['id']?>">Edit</a>
-                                        <a class="btn btn-danger" data-toggle="modal" data-target="#mm<?php echo $row['id']?>" >Delete</a>
-                                        <!-- The Modal -->
-                                        <div class="modal" id="mm<?php echo $row['id']?>">
-                                            <div class="modal-dialog">
-                                            <div class="modal-content">
-                                            
-                                                <!-- Modal Header -->
-                                                <div class="modal-header">
-                                                <h4 class="modal-title">Delete Confirmation!!!</h4>
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                </div>
-                                                
-                                                <!-- Modal body -->
-                                                <div class="modal-body">
-                                                    Are you sure to delete <b><?php echo $row['name'] ?></b> ?
-                                                </div>
-                                                
-                                                <!-- Modal footer -->
-                                                <div class="modal-footer">
-                                                <a href="delete-courses.php?id=<?php echo $row['id']?>" class="btn btn-success">Yes</a>
-                                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                                </div>
-                                                
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php } ?>
-                    </tbody>
-                </table>
+          <div class='row'>
+            <div class="col-lg-10">
+              <table class="table table-dark table-striped">
+                  <thead>
+                      <th>Name</th>
+                      <th>Code</th>
+                      <th>Type</th>
+                      <th>Action</th>
+                  </thead>
+                  <tbody>
+                      <?php 
+                          $qry="SELECT * FROM `courses`";
+                          $r=mysqli_query($conn,$qry);
+                          while($row=mysqli_fetch_array($r)){ ?>
+                              <tr>
+                                  <td> <?php echo $row['name']?> </td>
+                                  <td> <?php echo $row['code']?> </td>
+                                  <td> <?php echo $row['type']?> </td>
+                                  <td>
+                                      <a class="btn btn-primary" href="edit-courses.php?id=<?php echo $row['id']?>">Edit</a>
+                                      <a class="btn btn-danger" data-toggle="modal" data-target="#mm<?php echo $row['id']?>">Delete</a>
+                                      <!-- The Modal -->
+                                      <div class="modal" id="mm<?php echo $row['id']?>">
+                                          <div class="modal-dialog">
+                                          <div class="modal-content">
+                                              <!-- Modal Header -->
+                                              <div class="modal-header">
+                                              <h4 class="modal-title">Delete Confirmation!!!</h4>
+                                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                              </div>
+                                              
+                                              <!-- Modal body -->
+                                              <div class="modal-body">
+                                                  Are you sure to delete <b><?php echo $row['name'] ?></b> ?
+                                              </div>
+                                              
+                                              <!-- Modal footer -->
+                                              <div class="modal-footer">
+                                              <a href="delete-courses.php?id=<?php echo $row['id']?>" class="btn btn-success">Yes</a>
+                                              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                              </div>
+                                          </div>
+                                          </div>
+                                      </div>
+                                  </td>
+                              </tr>
+                          <?php } ?>
+                  </tbody>
+              </table>
             </div>
+          </div>
         </section>
       </section>
     </section>
