c2446e1057eeefd2d3ac865a51a8b33b3d827517
Tonmoy <tonmoy.cse.ctg@gmail.com>
2020-10-06 23:43:24 +0600

    modified: course-table.php, edit-courses.php created: teacher-table.php, edit-teachers.php, delete-teachers.php

--- admin/teacher-table.php
+++ admin/teacher-table.php
@@ -16,7 +16,7 @@
 <!DOCTYPE html>
   <html lang="en">
   <head>
-    <title>Admin (course table)</title>
+    <title>Admin (teacher table)</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include '../include/link.php' ?>
@@ -29,10 +29,10 @@
         <section class="wrapper">
           <div class="row">
             <div class="col-lg-12">
-              <h3 class="page-header"><i class="fa fa-laptop"></i>All Courses</h3>
+              <h3 class="page-header"><i class="fa fa-laptop"></i>All Teachers</h3>
               <ol class="breadcrumb">
                 <li><i class="fa fa-home"></i><a href="dashboard.php">Home</a></li>
-                <li><i class="fa fa-laptop"></i>Course Table</li>
+                <li><i class="fa fa-laptop"></i>Teacher Table</li>
               </ol>
             </div>
           </div>
@@ -40,21 +40,19 @@
                 <table class="table table-dark table-striped">
                     <thead>
                         <th>Name</th>
-                        <th>Code</th>
-                        <th>Type</th>
+                        <th>Email</th>
                         <th>Action</th>
                     </thead>
                     <tbody>
                         <?php 
-                            $qry="SELECT * FROM `courses`";
+                            $qry="SELECT * FROM users WHERE role = 'teacher' ";
                             $r=mysqli_query($conn,$qry);
                             while($row=mysqli_fetch_array($r)){ ?>
                                 <tr>
                                     <td> <?php echo $row['name']?> </td>
-                                    <td> <?php echo $row['code']?> </td>
-                                    <td> <?php echo $row['type']?> </td>
+                                    <td> <?php echo $row['email']?> </td>
                                     <td>
-                                        <a class="btn btn-primary" href="edit-courses.php?id=<?php echo $row['id']?>">Edit</a>
+                                        <a class="btn btn-primary" href="edit-teachers.php?id=<?php echo $row['id']?>">Edit</a>
                                         <a class="btn btn-danger" data-toggle="modal" data-target="#mm<?php echo $row['id']?>" >Delete</a>
                                         <!-- The Modal -->
                                         <div class="modal" id="mm<?php echo $row['id']?>">
@@ -74,7 +72,7 @@
                                                 
                                                 <!-- Modal footer -->
                                                 <div class="modal-footer">
-                                                <a href="delete-courses.php?id=<?php echo $row['id']?>" class="btn btn-success">Yes</a>
+                                                <a href="delete-teachers.php?id=<?php echo $row['id']?>" class="btn btn-success">Yes</a>
                                                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                                                 </div>
                                                 
